f31c59eb2d47fd817324bb8787850a689a59f53f
Roy <roykee.zano@gmail.com>
2022-02-14 19:47:47 +0200

    Arraylist Hazara

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+       ArrMax();
+       print("tamir");
+
     }
+
+    public static void ArrMax (){
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer>nambers= new ArrayList<Integer>();
+        for (int i = 0; i<5; i++){
+            nambers.add(in.nextInt());
+        }
+        print(Collections.max(nambers).toString());
+    }
+
+    public static void print(String a){
+        System.out.println(a);
+    }
+
+
 }
